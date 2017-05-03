fc80c1039154ec3eabb8e171cc75876a29ce7be1
Parthipan <Parthipan>
2017-05-04 01:10:34 +0530

    added JS Calculator Proj

--- Javascript Calculator/script.js
+++ Javascript Calculator/script.js
@@ -1,1 +1,22 @@
-/*Calculator Script*/
+const btns = document.querySelectorAll('.key');
+const display = document.querySelector('.display td');
+
+function getInput(){
+	this.classList.add('pressed');
+	this.addEventListener('transitionend',function(){
+		this.classList.remove('pressed');
+	});
+	switch(this.dataset.number){
+		case "=":
+			display.innerHTML = eval(display.innerHTML);
+			break;
+		case "C":
+			display.innerHTML = '';
+			break;
+		default:
+			display.innerHTML += this.dataset.number;
+	}
+}
+
+btns.forEach(btn => btn.addEventListener('click',getInput));
+
